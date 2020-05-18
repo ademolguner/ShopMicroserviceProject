2f260b46269178448a0ed1c02697a96e007694f6
ADEM OLGUNER <ademolguner@gmail.com>
2020-05-19 02:57:06 +0300

    Shop.ApiGateway.Api part 1

---
+++ OlgunerShop/Shop.ApiGateway.Api/Controllers/AdemOlgunerController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Shop.ApiGateway.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AdemOlgunerController : ControllerBase
+    {
+
+        private readonly ILogger<AdemOlgunerController> _logger;
+        public AdemOlgunerController(ILogger<AdemOlgunerController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string[] ShoppingOperationKeywords()
+        {
+            return new[] { "Adem", "Olguner", "Cicek Sepeti", "Proje" };
+        }
+    }
+}
